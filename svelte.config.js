27287ab51dd7749f6978b2785dc459a74679329c
jinwoo <rudwlsdn@naver.com>
2024-04-03 11:20:11 +0900

    설정 변경

--- svelte.config.js
+++ svelte.config.js
@@ -13,12 +13,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+			fallback: 'index.html',
+		}),
+		prerender: {entries: []}
 	}
 };
 export default config;
