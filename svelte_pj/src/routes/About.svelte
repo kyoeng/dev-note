be8cd9ffdf4311d52df638fc2881d670cdaa68b3
jinwoo <rudwlsdn@naver.com>
2024-04-03 10:47:26 +0900

    init

--- svelte_pj/src/routes/About.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    
-</script>
-
-<h2>About</h2>
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	base: "/dev_note/",
+	plugins: [sveltekit(), purgeCss()]
+});
